270198dce8d0fe72f0091d912c0104368c66de61
jtrelawney <ethan@bigbox>
2018-10-05 23:29:51 +0200

    accept-call in loop and fork the message processing

--- PC_cockpit_1/src/PC_cockpit_1.c
+++ PC_cockpit_1/src/PC_cockpit_1.c
@@ -1,5 +1,9 @@
 /* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+   The port number is passed as an argument
+   This version runs forever, forking off a separate
+   process for each connection
+   gcc server2.c -lsocket
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,27 +13,22 @@
 #include <strings.h>
 #include <unistd.h>
 
+void dostuff(int); /* function prototype */
 void error(char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-// setup standard call with port 8192
-
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
+     int sockfd, newsockfd, portno, clilen, pid;
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
+
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-
-     fprintf(stderr,"parameter port = %s\n",argv[1]);
-
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0)
         error("ERROR opening socket");
@@ -47,14 +46,42 @@ int main(int argc, char *argv[])
      fprintf(stderr,"waiting to accept message on port %i\n",portno);
      uint32_t clilen_match = (uint32_t) clilen;
 
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen_match); //&clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0;
+     while (1) {
+         newsockfd = accept(sockfd,
+               (struct sockaddr *) &cli_addr, &clilen_match); //&clilen);
+         if (newsockfd < 0)
+             error("ERROR on accept");
+         pid = fork();
+         if (pid < 0)
+             error("ERROR on fork");
+         if (pid == 0)  {
+             close(sockfd);
+             dostuff(newsockfd);
+             exit(0);
+         }
+         else {
+        	 fprintf(stderr,"parent here, closing socket and starting over\n");
+        	 close(newsockfd);
+         }
+     } /* end of while */
+     return 0; /* we never get here */
+}
+
+/******** DOSTUFF() *********************
+ There is a separate instance of this function
+ for each connection.  It handles all communication
+ once a connnection has been established.
+ *****************************************/
+void dostuff (int sock)
+{
+   int n;
+   char buffer[256];
+
+   bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (n < 0) error("ERROR reading from socket");
+   fprintf(stderr,"child here, closing socket and doing stuff ... \n");
+   printf("Here is the message: %s\n",buffer);
+   n = write(sock,"I got your message",18);
+   if (n < 0) error("ERROR writing to socket");
 }
